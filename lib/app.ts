77f471fef3bcc4c79573921fcd4c3bccaf3a7fb8
Kevin <codeskevin@gmail.com>
2019-01-01 23:55:59 -0700

    removed extraneous module.

--- lib/app.ts
+++ lib/app.ts
@@ -26,10 +26,10 @@ class App {
 
     private config(): void {
         // * support application/json type post data
-        this.app.use(bodyParser.json());
+        this.app.use(express.json());
 
         // * support application/x-www-form-urlencoded post data
-        this.app.use(bodyParser.urlencoded({ extended: false }));
+        this.app.use(express.urlencoded({ extended: false }));
 
         this.app.use(cookieParser());
 
